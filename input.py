181a8e63301603a5efa6c1fb88c41323cfedd5e7
lucas castro <lucas_o__castro@hotmail.com>
2025-04-04 00:27:39 -0300

    Frog on da pond

--- input.py
+++ input.py
@@ -4,11 +4,10 @@ from scene import Scene
 import functools
 
 class Input:
-    def __init__(self, scene: Scene, world_up: np.ndarray = np.array([0, 1, 0])) -> None:
+    def __init__(self, scene: Scene):
         self._delta_time = 0
         self.rotation_speed = 100
         self.scene = scene
-        self.world_up = world_up
 
         # Bind self to the callback
         glfw.set_key_callback(glfw.get_current_context(), functools.partial(self.key_event))
@@ -35,10 +34,10 @@ class Input:
     def handle_pad_input(self, key: int, action: int) -> None:
         lillypad = self.scene.lillypad
         if key == glfw.KEY_A and (action == glfw.PRESS or action == glfw.REPEAT):
-            lillypad.rotate_deg(self.rotation_speed * self._delta_time, self.world_up)
+            self.scene.rotate_object_deg(lillypad, self.rotation_speed * self._delta_time)
 
         if key == glfw.KEY_D and (action == glfw.PRESS or action == glfw.REPEAT):
-            lillypad.rotate_deg(-self.rotation_speed * self._delta_time, self.world_up)
+            self.scene.rotate_object_deg(lillypad, -self.rotation_speed * self._delta_time)
 
 
     def set_delta_time(self, delta_time: float) -> None:
--- scene.py
+++ scene.py
@@ -1,9 +1,10 @@
 from renderer import Renderer
 from objects.object import Object
 from typing import List
+from objects.scenery.floor import Floor
 from objects.actors.frog import Frog
-from objects.actors.tree import Tree
 from objects.actors.lillypad import LillyPad
+from objects.actors.tree import Tree
 from matrixmath import *
 import numpy as np
 
@@ -11,20 +12,44 @@ class Scene:
     def __init__(self, renderer: Renderer, world_rotation_rad: np.ndarray = np.array([0.0, 0.0, 0.0])):
         self.renderer = renderer
         self.world_rotation_rad = np.array(world_rotation_rad, dtype=np.float32)
+        self.world_up = (rotation_matrix_all(self.world_rotation_rad) @ np.array([0, 1, 0, 1], dtype=np.float32))[:-1]
+
         self.objects: List[Object] = self.gen_objects()
 
     def gen_objects(self) -> List[Object]:
+        self.floor = Floor()
+        self.floor.set_scale([1.5, 1, 1.5])
+        self.floor.set_rot_rad(self.world_rotation_rad)
+        self.floor.rotate_deg(90, [0, 1, 0])
+        self.floor.set_pos([0, 0, 0])
+        
         self.lillypad = LillyPad()
         self.lillypad.set_scale_single(.3)
         self.lillypad.set_rot_rad(self.world_rotation_rad)
+        self.translate_object(self.lillypad, np.array([0, .15, -.5]))
 
         self.frog = Frog()
         self.frog.set_scale_single(.36)
         self.frog.set_pos([0, .2, 0])
         self.lillypad.children.append(self.frog)
         
-        return [self.lillypad]
+        return [self.floor, self.lillypad]
+    
+    def rotate_object_deg(self, obj: Object, angle_deg: np.ndarray) -> None:
+        angle_rad = np.deg2rad(angle_deg)
+        self.rotate_object_rad(obj, angle_rad)
+    
+    def rotate_object_rad(self, obj: Object, angle_rad: np.ndarray) -> None:
+        axis = self.world_up
+        position = np.array(obj.position) # clone because it will be modified
+        obj.translate(-position)
+        obj.rotate_rad(angle_rad, axis)
+        obj.translate(position)
 
+    def translate_object(self, obj: Object, delta: np.ndarray) -> None:
+        delta = [*delta, 1]
+        delta = (rotation_matrix_all(self.world_rotation_rad) @ delta)[:-1]
+        obj.translate(delta)
 
     def render_scene(self) -> None:
         for obj in self.objects:
