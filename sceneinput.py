6930ca4abb0f7d0802547de2fbcb489576c47322
lucas castro <lucas_o__castro@hotmail.com>
2025-05-23 15:06:20 -0300

    Better control of lights  in sceneinput

--- sceneinput.py
+++ sceneinput.py
@@ -14,28 +14,38 @@ class SceneInput:
         self.renderer = renderer
 
         input.register_key_callback(glfw.KEY_P, renderer.toggle_wireframe)
-
-        # ambient color
-        self.key_ambient_color_down = glfw.KEY_KP_2
-        self.key_ambient_color_up = glfw.KEY_KP_8
-        self.key_ambient_color_reset = glfw.KEY_KP_5
-        self.key_ambient_color_speed = 5
-        input.register_key_callback(self.key_ambient_color_reset, renderer.lit_program.reset_ambient_color)
+        self._setup_editable_lights()
 
     def update(self, delta_time: float) -> None:
         for element in self.scene.container.children:
             element.update(self.input, delta_time)
         
-        self.handle_ambient_color(delta_time)
+        self._update_editable_lights(delta_time)
     
-    def handle_ambient_color(self, delta_time: float):
-        color_delta = None
-        if self.input.is_key_held(self.key_ambient_color_up):
-            color_delta = np.ones(3, dtype=np.float32)
-        elif self.input.is_key_held(self.key_ambient_color_down):
-            color_delta = -np.ones(3, dtype=np.float32)
+    def _setup_editable_lights(self):
+        # Callbacks de input para controlar as luzes
+        def on_light_key_pressed(i):
+            print(f"Light {i} selected")
+            self.current_light = self.editable_lights[i]
+        def on_reset_key_pressed():
+            self.current_light.reset()
+        
+        # Define as teclas para controlar as luzes
+        self.key_light_down = glfw.KEY_KP_2
+        self.key_light_up = glfw.KEY_KP_8
+        self.key_light_reset = glfw.KEY_KP_5
+        self.light_delta_speed = 5
+
+        # Pega todas as luzes da cena e defaulta para a ambiente
+        self.editable_lights = self.scene.get_all_lights()
+        self.current_light = self.editable_lights[0]
+
+        # Registra as callbacks para cada luz
+        for i in range(min(len(self.editable_lights), 9)):
+            self.input.register_key_callback(glfw.KEY_1 + i, lambda i=i: on_light_key_pressed(i))
+        self.input.register_key_callback(self.key_light_reset, on_reset_key_pressed)
+
+    def _update_editable_lights(self, delta_time: float):
+        delta = self.input.get_1d_axis(self.key_light_up, self.key_light_down)
+        self.current_light.intensity += delta * delta_time * self.light_delta_speed
             
-        if color_delta is not None:
-            color = self.renderer.lit_program.ambient_light_color
-            color += color_delta * delta_time * self.key_ambient_color_speed
-            self.renderer.lit_program.set_ambient_color(color)
